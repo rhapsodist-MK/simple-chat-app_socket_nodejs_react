7cfada96d56ab68bef38d4a7ac4e4a2db0d6b696
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-24 20:03:28 +0900

    set server.js

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+server.listen(PORT, () => { console.log(`Server has started on port ${PORT}`) })
