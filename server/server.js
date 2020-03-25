e43e130813c2214a58ec13818f058442bd764fd9
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-25 17:20:41 +0900

    add router

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+const router = require('./router')
+
+app.use(router)
 
 server.listen(PORT, () => { console.log(`Server has started on port ${PORT}`) })
