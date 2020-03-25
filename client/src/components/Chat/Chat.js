e6c9499ee289164600b50a0fba9f006f18dbef2d
김민우 <rhapsodist@gimmin-uui-MacBookPro.local>
2020-03-26 00:50:29 +0900

    link socket between client chat and server socket

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,9 @@ let socket;
 export default ({ location }) => {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+
   const ENDPOINT = `localhost:5000`
 
   useEffect(() => {
@@ -17,9 +20,39 @@ export default ({ location }) => {
     setName(name)
     setRoom(room)
 
-    socket.emit('join', { name, room})
+    socket.emit('join', { name, room }, () => { // join이라는 이름으로 name, room을 서버로 토스하고, callback을 보내 서버에서 처리를 client에서 반영시킬 수 있다
+      
+    })
+
+    return () => {
+      socket.emit('disconnect')
+      socket.off()
+    }
   }, [ENDPOINT, location.search]) // 오직 배열안의 값들이 변할때만 실행시킴
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+    })
+  }, [messages])
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    if (message) socket.emit('sendMessage', message, () => setMessage(''))
+  }
+
+  console.log(message, messages)
+
   return (
-    <h1>Chat</h1>
+    <div className="outerContainer">
+      <div className="container">
+        <input 
+          type="text" 
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null}
+        />
+      </div>
+    </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -11,7 +13,28 @@ const io = socketio(server)
 const router = require('./router')
 
 io.on('connection', (socket) => {
-  console.log('we have a new connection!!!!!!!!')
+  socket.on('join', ({ name, room }, callback) => { // client emit 쪽에서 넘겨준 string을 event 이름으로 지정하여 실행
+    const { error, user } = addUser({ id: socket.id, name, room })
+
+    if (error) return callback(error)
+
+    // 입장한 유저에게 메세지를 보냄
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+    // 입장한 유저 이외에 방에 존재하는 유저들에게 메세지를 보냄
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`})
+
+    socket.join(user.room)
+    callback()
+  })
+
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id)
+
+    io.to(user.room).emit('message', { user: user.name, text: message})
+    callback()
+  })
+
 
   socket.on('disconnect', () => {
     console.log('user had left!')
